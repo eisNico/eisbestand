29cdd1f2cdfa0af2295a02bf1cef34a29124328c
eisNico <kgkristjan1@gmail.com>
2025-04-30 02:23:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,37 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("eis-store").then(cache => {
-      return cache.addAll(["./", "./index.html", "./style.css", "./script.js", "./manifest.json"]);
+const CACHE_NAME = 'eistabelle-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
