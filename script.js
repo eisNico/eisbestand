d5c34fe37ab1a235d77968de6c6d22b996754b75
eisNico <kgkristjan1@gmail.com>
2025-04-30 14:34:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const correctPassword = "1234";
+const correctPassword = "eisp1";
 const storageKey = "eistabelleData";
 const defaultSorten = [
   "Schokolade", "Vanille", "Erdbeer", "Himmelblau", "Lila",
